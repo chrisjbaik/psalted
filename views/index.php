659e8e55e708cda83a0ab20a6f2a2d08ff3b7315
Chris Baik <chrisjbaik@gmail.com>
2013-08-17 19:08:32 -0400

    Remove facebook buttons, old files

--- views/index.php
+++ views/index.php
@@ -4,7 +4,7 @@
     <input type="text" name="email" id="text-basic" placeholder="Email" value="<?php if (isset($email)) { echo $email; }?>">
     <input type="password" name="password" id="password" placeholder="Password" value="" autocomplete="off">
     <input type="submit" value="Log In" data-role="button" />
-    <a href="#" data-theme="b" data-role="button">Log In w/ Facebook</a>
+    <!--<a href="#" data-theme="b" data-role="button">Log In w/ Facebook</a>-->
   </form>
 </div>
 <?php include_once('../views/includes/footer_jqm.php'); ?>
