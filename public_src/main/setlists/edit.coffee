8c180815c213eda45fefc3ced9a352cea9b5b00c
Chris Baik <chrisjbaik@gmail.com>
2013-10-31 20:33:36 -0400

    Fix setlist listview filter bug

---
+++ public_src/main/setlists/edit.coffee
@@ -0,0 +1,54 @@
+$ = require('jquery')
+$.mobile = require('jquery-mobile')
+
+$(document).delegate "#setlists-edit", "pageinit", ->
+  $("#setlists-new-song-choices").on "filterablebeforefilter", (e, data) ->
+    $ul = $( this )
+    $input = $( data.input )
+    value = $input.val()
+    html = ""
+    $ul.html( "" )
+    if value && value.length > 2
+      $ul.html( "<li><div class='ui-loader'><span class='ui-icon ui-icon-loading'></span></div></li>" )
+      $ul.listview( "refresh" )
+      $.ajax
+        url: "/search/song_titles/" + $input.val()
+        dataType: "json"
+      .then (response) ->
+        $.each response, (i, val) ->
+          if $('#setlists-new-songs li[data-id=' + val.id + ']').length is 0
+            html += "<li><a href='#' data-artist='" + val.artist + "' data-id='" + val.id + "' data-key='" + val.key + "'>" + val.title + "</a></li>"
+        $ul.html(html)
+        $ul.listview("refresh")
+        $ul.trigger("updatelayout")
+
+  $(document).on 'click', '#setlists-new-song-choices a[data-id]', (e) ->
+    $('#setlists-song-chosen-by-popup').popup('open')
+    $('#setlists-song-chosen-by-popup').attr('data-id', $(this).attr('data-id'))
+    $('#setlists-song-chosen-by-popup').attr('data-title', $(this).text())
+    $('#setlists-song-chosen-by-popup').attr('data-artist', $(this).attr('data-artist'))
+    $('#setlists-song-chosen-by-popup h2').text($(this).text())
+    $('#setlists-songs-key').val($(this).attr('data-key') || 0)
+    $('#setlists-songs-key').selectmenu('refresh')
+
+  $(document).on 'click', '#setlist-chosen-by-submit', (e) ->
+    if $('#setlists-new-songs li[data-id=' + $('#setlists-song-chosen-by-popup').attr('data-id') + ']').length is 0
+      nextIndex = $('#setlists-new-songs li[data-id]').length
+      appendHtml = "<li data-theme='c' data-id='" + $('#setlists-song-chosen-by-popup').attr('data-id') + "'>" +
+        "<a href='#'>" + $('#setlists-song-chosen-by-popup').attr('data-title') +
+        " (" + $('#setlists-song-chosen-by-popup').attr('data-artist') + ") </a>" +
+        "<a href='#' class='remove-song'>Remove Song</a>" +
+        "<input type='hidden' name='songs[" + nextIndex + "][id]' value='" +
+        $('#setlists-song-chosen-by-popup').attr('data-id') +
+        "' /><input type='hidden' name='songs[" + nextIndex + "][chosen_by]' value='" +
+        $('#setlists-song-chosen-by-select').val() + "' />" +
+        "<input type='hidden' name='songs[" + nextIndex + "][key]' value='" +
+        $('#setlists-songs-key').val() + "' /></li>"
+      $('#setlists-new-songs').append appendHtml
+      $('#setlists-new-songs').listview('refresh')
+      $('#setlists-new-song-choices-box .ui-input-clear').click()
+      $('#setlists-new-song-choices').html('')
+      $('#setlists-song-chosen-by-popup').popup('close')
+
+  $(document).on 'click', '.remove-song', (e) ->
+    $(this).closest('li').remove()
--- views/setlists/edit.php
+++ views/setlists/edit.php
@@ -1,6 +1,6 @@
 <?php include_once('../views/includes/header.php'); ?>
 <div data-role="content">
-  <form id="setlists-new" method="post" data-ajax='false'>
+  <form id="setlists-new-form" method="post" data-ajax='false'>
     <label for="setlist-title" class="ui-hidden-accessible">Setlist Name</label>
     <input type="text" name="title" id="setlist-title" placeholder="Setlist Name" value="<?php if (!empty($setlist->title)) { echo $setlist->title; } ?>">
     <label for="setlist-date" class="ui-hidden-accessible">Setlist Date</label>
@@ -59,64 +59,5 @@
       <a href='#' id='setlist-chosen-by-submit' data-theme='b' data-role='button'>Add Song</a>
     </div>
   </div>
-  <script>
-    $("#setlists-new-song-choices").on("listviewbeforefilter", function ( e, data ) {
-      var $ul = $( this ),
-          $input = $( data.input ),
-          value = $input.val(),
-          html = "";
-      $ul.html( "" );
-      if ( value && value.length > 2 ) {
-        $ul.html( "<li><div class='ui-loader'><span class='ui-icon ui-icon-loading'></span></div></li>" );
-        $ul.listview( "refresh" );
-        $.ajax({
-          url: "/search/song_titles/" + $input.val(),
-          dataType: "json"
-        })
-        .then( function ( response ) {
-          $.each( response, function ( i, val ) {
-            if ($('#setlists-new-songs li[data-id=' + val.id + ']').length === 0) {
-              html += "<li><a href='#' data-artist='" + val.artist + "' data-id='" + val.id + "' data-key='" + val.key + "'>" + val.title + "</a></li>";
-            }
-          });
-          $ul.html( html );
-          $ul.listview( "refresh" );
-          $ul.trigger( "updatelayout");
-        });
-      }
-    });
-    $(document).on('click', '#setlists-new-song-choices a[data-id]', function (e) {
-      $('#setlists-song-chosen-by-popup').popup('open');
-      $('#setlists-song-chosen-by-popup').attr('data-id', $(this).attr('data-id'));
-      $('#setlists-song-chosen-by-popup').attr('data-title', $(this).text());
-      $('#setlists-song-chosen-by-popup').attr('data-artist', $(this).attr('data-artist'));
-      $('#setlists-song-chosen-by-popup h2').text($(this).text());
-      $('#setlists-songs-key').val($(this).attr('data-key') || 0);
-      $('#setlists-songs-key').selectmenu('refresh');
-    });
-    $(document).on('click', '#setlist-chosen-by-submit', function (e) {
-      if ($('#setlists-new-songs li[data-id=' + $('#setlists-song-chosen-by-popup').attr('data-id') + ']').length === 0) {
-        var nextIndex = $('#setlists-new-songs li[data-id]').length;
-        $('#setlists-new-songs').append(
-          "<li data-theme='c' data-id='" + $('#setlists-song-chosen-by-popup').attr('data-id') + "'>"
-          + "<a href='#'>" + $('#setlists-song-chosen-by-popup').attr('data-title')
-          + " (" + $('#setlists-song-chosen-by-popup').attr('data-artist') + ") </a>"
-          + "<a href='#' class='remove-song'>Remove Song</a>"
-          + "<input type='hidden' name='songs[" + nextIndex + "][id]' value='"
-          + $('#setlists-song-chosen-by-popup').attr('data-id')
-          + "' /><input type='hidden' name='songs[" + nextIndex + "][chosen_by]' value='"
-          + $('#setlists-song-chosen-by-select').val() + "' />"
-          + "<input type='hidden' name='songs[" + nextIndex + "][key]' value='"
-          + $('#setlists-songs-key').val() + "' /></li>");
-        $('#setlists-new-songs').listview('refresh');
-        $('#setlists-new-song-choices-box .ui-input-clear').click();
-        $('#setlists-new-song-choices').html('');
-        $('#setlists-song-chosen-by-popup').popup('close');
-      }
-    });
-    $(document).on('click', '.remove-song', function (e) {
-      $(this).closest('li').remove();
-    });
-  </script>
 </div>
 <?php include_once('../views/includes/footer.php'); ?>
