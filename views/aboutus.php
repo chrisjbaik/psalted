650767edd7c34c3607aa6e9e83077a04da994960
Hong Yoon Kim <hongyoon@umich.edu>
2013-11-16 14:32:49 -0500

    about us page

---
+++ views/aboutus.php
@@ -0,0 +1,32 @@
+<?php include_once('../views/includes/header.php'); ?>
+<div data-role="content">
+	<div>
+		<img src="img/aboutus.jpg" align = "left" style="padding-right:15px; width: 50%; height: 50%;" >
+			<p> We all attend the same church in Ann Arbor, Michigan. (check out <strong> <a href="http://hmcc.net">hmcc.net </a> </strong>) 
+				<br> 
+				<br> 
+				We developed this site to streamline a song sheet preparation process. 
+				<br>
+				<br>  			
+				If you have any questions, concerns, or suggestions, 
+				<br>
+				please contact <a href="mailto:bacon@psalted.com">bacon@psalted.com</a>
+				<br>
+				<br>
+				This site is being developed and maintained by <strong>over-worked and under-paid</strong> volunteers, who work during the day to pay the bills,
+				 and we need copious amounts of <a href="http://en.wikipedia.org/wiki/Caffeine">caffeine</a> and sugary snacks when we code. 
+				We will be very thankful if you can make a <strong>small donation</strong> to support the future development. 
+				<br>
+				<br>
+				Please contact <a href="mailto:ihavemoney@psalted.com">ihavemoney@psalted.com</a> if you would like to donate.  
+				<br>
+				<br>
+				Thanks, 
+
+
+
+			</p>
+	</div>
+
+</div>
+<?php include_once('../views/includes/footer.php'); ?>
