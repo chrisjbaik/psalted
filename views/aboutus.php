3713bd399ba49604e7d9af790312947dca1076f3
Varoot Phasuthadol <varoot@gmail.com>
2014-02-20 15:07:49 -0500

    Restyle About Us

--- views/aboutus.php
+++ views/aboutus.php
@@ -1,32 +1,27 @@
 <?php include_once('../views/includes/header.php'); ?>
 <div data-role="content">
-	<div>
-		<img src="img/aboutus.jpg" align = "left" style="padding-right:15px;" >
-			<p> We all attend the same church in Ann Arbor, Michigan. (check out <strong> <a href="http://hmcc.net">hmcc.net </a> </strong>) 
-				<br> 
-				<br> 
-				We developed this site to streamline a song sheet preparation process. 
-				<br>
-				<br>  			
-				If you have any questions, concerns, or suggestions, 
-				<br>
-				please contact <a href="mailto:bacon@psalted.com">bacon@psalted.com</a>
-				<br>
-				<br>
-				This site is being developed and maintained by <strong>overworked and underpaid</strong> volunteers, who work during the day to pay the bills,
-				 and we need copious amounts of <a href="http://en.wikipedia.org/wiki/Caffeine">caffeine</a> and sugary snacks when we code. 
-				We will be very thankful if you can make a <strong>small donation</strong> to support the future development. 
-				<br>
-				<br>
-				Please contact <a href="mailto:ihavemoney@psalted.com">ihavemoney@psalted.com</a> if you would like to donate.  
-				<br>
-				<br>
-				Thanks, 
-
-
-
+	<div class="ui-grid-a ui-responsive">
+		<div class="ui-block-a space-right">
+			<img src="img/aboutus.jpg" alt="Varoot and Chris passing out during our development session" class="img-responsive">
+		</div>
+		<div class="ui-block-b">
+			<p>We all attend the same church in Ann Arbor, Michigan. (check out <a href="http://hmcc.net" target="_blank">hmcc.net</a>)</p>
+			<p>We developed this site to streamline a song sheet preparation process.</p>
+			<p>If you have any questions, concerns, or suggestions,
+				<br>please contact <a href="mailto:bacon@psalted.com">bacon@psalted.com</a>
 			</p>
+			<p>This site is being developed and maintained by <strong>overworked and underpaid</strong> volunteers, who work during the day to pay the bills,
+				and we need copious amounts of <a href="http://en.wikipedia.org/wiki/Caffeine" target="_blank">caffeine</a> and sugary snacks when we code. 
+				We will be very thankful if you can make a <strong>small donation</strong> to support the future development. 
+			</p>
+			<p>
+				Please contact <a href="mailto:ihavemoney@psalted.com">ihavemoney@psalted.com</a> if you would like to donate.  
+			</p>
+			<p>
+				Thanks,
+				<br>Team Sawadicop
+			</p>
+		</div>
 	</div>
-
 </div>
 <?php include_once('../views/includes/footer.php'); ?>
